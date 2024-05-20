18df0504d79621d791a03ff14147b65d46455437
Fatih Egriboz <egriboz@gmail.com>
2024-05-20 16:23:05 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [weatherData, setWeatherData] = useState(null)
-  const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('İstanbul')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,29 +23,44 @@ function App() {
 
   const handleLocationChange = () => (e) => {
     setLocation(e.target.value)
-    console.log(e.target.value)
+    // console.log(e.target.value)
   }
-  console.log('weatherData', weatherData)
+  // console.log('weatherData', weatherData)
   return (
     <>
       <div>
         <h1>Weather App</h1>
-        <input
-          type="text"
-          value={location}
-          // onChange={(e) => setLocation(e.target.value)}
-          onChange={handleLocationChange()}
-        />
+        <div className='input-container'>
+          <input
+            type="text"
+            value={location}
+            // onChange={(e) => setLocation(e.target.value)}
+            onChange={handleLocationChange()}
+          />
+        </div>
       </div>
-      <div>
+      {/* <div>
         {weatherData && (
-          <>
+          <div>
             <h2>{weatherData.location.name}</h2>
             <h3>{weatherData.current.temp_c}°C</h3>
             <img src={weatherData.current.condition.icon} alt={weatherData.current.condition.text} />
             <p>{weatherData.current.condition.text}</p>
-          </>
+          </div>
         )}
+      </div> */}
+      <div><h2>{location}</h2></div>
+      <div className='grid-contanier'>
+
+        {weatherData && weatherData.forecast.forecastday.map((day) => (
+          <div className='grid-contanier__inner' key={day.date}>
+            <h3>{day.date}</h3>
+            <img src={day.day.condition.icon} alt={day.day.condition.text} />
+            <p>{day.day.condition.text}</p>
+            <p>{day.day.maxtemp_c}°C</p>
+            <p>{day.day.mintemp_c}°C</p>
+          </div>
+        ))}
       </div>
     </>
   )
